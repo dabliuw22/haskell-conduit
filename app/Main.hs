edd8a8e18c609ee624bf613814ecd4437688ca7e
Will Leyton <wleylop22@gmail.com>
2021-03-11 08:03:08 -0500

    Add Main

--- app/Main.hs
+++ app/Main.hs
@@ -1,6 +1,14 @@
 module Main where
 
-import Lib
+import qualified Basic.Conduit as B
+import Conduit (runConduitRes, sinkList, (.|))
+import qualified Files.Conduit as F
+import System.Directory (getCurrentDirectory)
+import System.FilePath ((</>))
 
 main :: IO ()
-main = someFunc
+main = do
+  _ <- B.run
+  directory <- (</> "files/in") <$> getCurrentDirectory
+  l <- runConduitRes $ F.read directory F.Txt .| sinkList
+  print l
